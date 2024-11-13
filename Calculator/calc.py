463594ffebb8bc65ad5d17eb07cf3120d31170ed
OnePailas <tuwgvfc@gmail.com>
2024-11-13 22:38:53 +0500

    Add files via upload

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -1,1 +1,22 @@
+print('Привет, этот калькулятор поможет вычесть ответ двух переменных')
 
+oper = '  +   -   *   /'
+print(f'Выбери действие: {oper}')
+operation = input()
+
+
+if operation in ['+', '-', '*', '/']:
+    num1 = float(input('Введите первое число: '))
+    num2 = float(input('Введите второе число: '))
+    if operation == '+':
+        print('Ответ: ', num1 + num2)
+    elif operation == '-':
+        print('Ответ: ', num1 - num2)
+    elif operation == '*':
+        print('Ответ: ', num1 * num2)
+    elif operation == '/':
+        if num2 == 0:
+            print('На ноль делить нельзя')
+            num2 = float(input('Введите еще раз второе число: '))
+        if num2 > 0:
+            print('Ответ: ', num1 / num2)
